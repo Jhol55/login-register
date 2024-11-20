40143334033df11fec651d5044510d346c168c14
Jhonathan <jhonathan_galhardo@hotmail.com>
2024-11-20 11:44:36 -0300

    refactor(input-choice): removed old checkbox input

--- src/components/ui/input-choice/input-choice.type.d.ts
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-export interface InputChoiceProps
-  extends InputHTMLAttributes<HTMLInputElement> {
-  fieldName: keyof Record<string, (e: ChangeEvent<HTMLInputElement>) => void>;
-  type?: 'checkbox' | 'radio';
-  includeInForm?: boolean;
-}
