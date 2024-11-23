730c75c06a598ab03949d972bf0bd076613d942a
Jhonathan <jhonathan_galhardo@hotmail.com>
2024-11-23 00:32:50 -0300

    test(input-otp): added tests

---
+++ src/components/ui/input-otp/input-otp.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, fireEvent, screen } from '@testing-library/react';
+import { InputOTP } from './input-otp';
+import { useForm } from '@/hooks/use-form';
+
+jest.mock('@/hooks/use-form', () => ({
+  useForm: jest.fn(),
+}));
+
+describe('InputOTP Component', () => {
+  const setUp = (props = {}) => {
+    (useForm as jest.Mock).mockReturnValue({
+      register: jest.fn().mockReturnValue({
+        ref: jest.fn(),
+        onChange: jest.fn(),
+      }),
+      setValue: jest.fn(),
+    });
+
+    return render(<InputOTP fieldName="test" length={6} {...props} />);
+  };
+
+  it('should render the input element correctly', () => {
+    setUp();
+
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+  });
+
+  it('should update the value when typing', () => {
+    const { container } = setUp();
+
+    const input = container.querySelector('input') as HTMLInputElement;
+    fireEvent.change(input, { target: { value: 'test value' } });
+
+    expect(input.value).toBe('test value');
+  });
+
+  it('should call onChange prop when typing', () => {
+    const onChangeMock = jest.fn();
+    const { container } = setUp({ onChange: onChangeMock });
+
+    const input = container.querySelector('input') as HTMLInputElement;
+    fireEvent.change(input, { target: { value: 'test value' } });
+
+    expect(onChangeMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should apply the mask schema function to input value when provided', () => {
+    const maskSchema = {
+      test: (e: React.ChangeEvent<HTMLInputElement>) => e.target.value.toUpperCase(),
+    };
+
+    const setValue = jest.fn()
+
+    const onChangeMock = jest.fn((e: React.ChangeEvent<HTMLInputElement>) => {
+      const type = e.target.type; 
+      if (!["checkbox", "radio"].includes(type)) {
+        const maskedValue = maskSchema.test(e) ?? e.target.value;
+        setValue("test", maskedValue);
+      }
+    });
+  
+    const { container } = setUp({ onChange: onChangeMock });
+    const input = container.querySelector('input') as HTMLInputElement;
+
+    fireEvent.change(input, { target: { value: 'test value' } });
+
+    expect(setValue).toHaveBeenCalledWith('test', 'TEST VALUE');
+  });
+});
